2bc0e667e9df2843bb6b706be33317fb0b2c88aa
cltimothe <timothe.clement@ynov.com>
2024-11-28 10:59:14 +0100

    set up des pages webs
    lien entre html et affichage go

--- source/main.go
+++ source/main.go
@@ -3,18 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
-func hangmanHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "This is the Hangman game!")
-}
-
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Welcome to Hangman-Web!")
-	})
 
-	http.HandleFunc("/hangman", hangmanHandler)
+	// Debug: Check if accueil.html exists
+	if _, err := os.Stat("./web/accueil.html"); err != nil {
+		fmt.Println("accueil.html not found:", err)
+	} else {
+		fmt.Println("accueil.html is ready to be served!")
+	}
+
+	fileServer := http.FileServer(http.Dir("./web"))
+	http.Handle("/web/", http.StripPrefix("/web/", fileServer))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/accueil.html")
+	})
 
 	fmt.Println("Server is running at http://localhost:8080/")
 	http.ListenAndServe(":8080", nil)
